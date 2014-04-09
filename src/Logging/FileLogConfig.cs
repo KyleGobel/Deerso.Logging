4b22595c9df58f801556284e6eed65eaf8bde127
Kyle Gobel <kgobel@gmail.com>
2014-04-09 00:52:06 -0400

    someChanges

--- src/Logging/FileLogConfig.cs
+++ src/Logging/FileLogConfig.cs
@@ -4,12 +4,14 @@ namespace Logging
 {
     public class FileLogConfig
     {
-        public Lazy<FileLogConfig> Instance = new Lazy<FileLogConfig>(() => new FileLogConfig()); 
-        private FileLogConfig()
+        public Lazy<FileLogConfig> Instance = new Lazy<FileLogConfig>(() => new FileLogConfig());
+
+        static FileLogConfig()
         {
             LogDirectory = "Logs";
             LogExtension = ".log";
-        } 
+        }
+        private FileLogConfig(){}
         public static string LogDirectory { get; set; }
         public static string LogExtension { get; set; }
     }
