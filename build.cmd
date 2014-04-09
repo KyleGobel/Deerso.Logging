6f496191e1414d088453d799b5ed1bb3e7c94010
Kyle Gobel <kgobel@gmail.com>
2014-04-08 21:17:20 -0400

    attmpt at new build.cmd

--- build.cmd
+++ build.cmd
@@ -1,1 +1,19 @@
-Nuget\NugetPack.cmd
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Build
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Deerso.Logging.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+if not "%errorlevel%"=="0" goto failure
+
+REM Package
+mkdir Build
+cmd /c %nuget% pack "NuGet\Deerso.Logging.nuspec" -symbols -o Build -p Configuration=%config% %version%
+if not "%errorlevel%"=="0" goto failure
